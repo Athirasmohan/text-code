49911acfc35b1ce6622d99f349d793dfcc4e49ea
Athirasmohan <143090724+Athirasmohan@users.noreply.github.com>
2023-11-01 08:42:25 +0400

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,9 +19,6 @@ export default function Navbar(props) {
         <li className="nav-item">
           <Link className="nav-link active" aria-current="page" to="/about">About</Link>
         </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/">{props.title}</Link>
-        </li>
       </ul>
       {/* <form className="d-flex" role="search">
         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
